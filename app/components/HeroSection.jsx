74a648254befc52e46e470adc20fd66a336653e1
Vincenzo Fehring <vinfehring@proton.me>
2024-02-08 02:48:53 -0600

    Forgot to escape the apostophe

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
     <section>
       <div className="grid grid-cols-1 lg:grid-cols-12">
         <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I'm Vincenzo</h1>
+          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">Hello, I&apos;m Vincenzo</h1>
           <p className="text-[#ADB7BE] text-lg lg:text-xl">
           Dedicated professional with demonstrated strengths in customer service,
     time management and trend tracking. Good at troubleshooting problems
